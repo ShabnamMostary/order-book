9392e63dc4d984488788099c8f517c825f1100f0
shabnam <shabnam.mostary@gmail.com>
2020-10-18 01:13:55 -0400

    updated  the code to handle these additional cases: 1.partially fulfills an order, removes the matching order and adds the remainder of the order to the book when the book contains a matching order of a smaller quantity 2.uses two existing orders to completely fulfill an order, removing the matching orders from the book 3.uses two existing orders to completely fulfill an order, removing the first matching order from the book and reducing the second 4. uses two existing orders to partially fulfill an order, removing the matching orders from the book and reducing the incoming order  before adding it to the book

--- orderBook.js
+++ orderBook.js
@@ -6,22 +6,53 @@ function reconcileOrder(existingBook, incomingOrder) {
   } else if (n == 1 && existingBook[0].type == incomingOrder.type || existingBook[0].price != incomingOrder.price) {
     updatedOrder.push(existingBook[0], incomingOrder)
   } else if (n > 1) {
+    var matchingOrder = []
     for (var i = 0; i < n; i++) {
       if (existingBook[i].type == incomingOrder.type) {
         updatedOrder.push(existingBook[i])
-      } else if (existingBook[i].quantity > incomingOrder.quantity && existingBook[i].price == incomingOrder.price) {
-        var diff = existingBook[i].quantity - incomingOrder.quantity
-        var adjustment = { type: existingBook[i].type, quantity: diff, price: existingBook[i].price }
+      } else {
+
+        matchingOrder.push(existingBook[i])
+      }
+    }
+    if (matchingOrder.length == 1) {
+      if (matchingOrder[0].quantity > incomingOrder.quantity && matchingOrder[0].price == incomingOrder.price) {
+        var diff = matchingOrder[0].quantity - incomingOrder.quantity
+        var adjustment = { type: matchingOrder[0].type, quantity: diff, price: matchingOrder[0].price }
+        updatedOrder.push(adjustment)
+      } else if (matchingOrder[0].quantity < incomingOrder.quantity && matchingOrder[0].price == incomingOrder.price) {
+        diff = incomingOrder.quantity - matchingOrder[0].quantity
+        adjustment = { type: incomingOrder.type, quantity: diff, price: incomingOrder.price }
         updatedOrder.push(adjustment)
       }
+    } else {
+      if (matchingOrder[0].quantity + matchingOrder[1].quantity >= incomingOrder.quantity && matchingOrder[0].price == incomingOrder.price
+        && matchingOrder[1].price == incomingOrder.price) {
+        diff = matchingOrder[0].quantity + matchingOrder[1].quantity - incomingOrder.quantity
+        if (diff != 0) {
+          adjustment = {
+            type: matchingOrder[0].type, quantity: diff, price: matchingOrder[0].price
+          }
+          updatedOrder.push(adjustment)
+        }
+      } else if (matchingOrder[0].quantity + matchingOrder[1].quantity < incomingOrder.quantity && matchingOrder[0].price == incomingOrder.price
+        && matchingOrder[1].price == incomingOrder.price) {
+        diff = incomingOrder.quantity - (matchingOrder[0].quantity + matchingOrder[1].quantity)
+        if (diff != 0) {
+          adjustment = {
+            type: incomingOrder.type, quantity: diff, price: incomingOrder.price
+          }
+          updatedOrder.push(adjustment)
+        }
+      }
+
+
     }
 
   }
-  updatedOrder.push(updatedOrder.splice(updatedOrder.indexOf(adjustment), 1)[0])
   return updatedOrder
 
+
 }
-
-
 
 module.exports = reconcileOrder
